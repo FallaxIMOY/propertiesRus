a13425ef64bb6bfbb45514588c52f039827d742e
EmmanuelAdio <emmanueladio065@gmail.com>
2024-08-01 15:32:14 +0100

    initial commit

---
+++ propertiesrus/src/backend_setup.js
@@ -0,0 +1,17 @@
+const http = require('node:http');
+const { url } = require('node:inspector');
+const hostname = '127.0.0.1';
+const port = 3000;
+var url= require(url)
+var properties_db = require("mongodb://localhost:27017/properties_db");
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Server Started!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
