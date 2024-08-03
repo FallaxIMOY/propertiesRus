42e50c1d0fcc5cdb03c276eee407fad535e8383f
EmmanuelAdio <emmanueladio065@gmail.com>
2024-08-03 17:06:50 +0100

    before u left

--- propertiesrus/src/backend_setup.js
+++ propertiesrus/src/backend_setup.js
@@ -1,9 +1,9 @@
 const http = require('node:http');
-const { url } = require('node:inspector');
+//const { url } = require('node:inspector');
 const hostname = '127.0.0.1';
 const port = 3000;
-var url= require(url)
-var properties_db = require("mongodb://localhost:27017/properties_db");
+//var url= require(url)
+var properties_db = require("./properties_db");
 
 
 const server = http.createServer((req, res) => {
